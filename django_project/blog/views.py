6355b6236813a9ed710f3119ddb25a6e0ddd597d
Saurabh Tripathi <saurabhtripathi9899@gmail.com>
2019-06-20 22:51:45 +0530

    Update views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
+posts = [
+	{
+		'author': 'coreyMS',
+		'title': 'blog post 1',
+		'content': 'first post content',
+		'date_posted' :'August 27,2018'
+	},
 
+	{
+		'author': 'jane doe',
+		'title': 'blog post 2',
+		'content': 'first post content',
+		'date_posted': 'August 28,2018'
+	}
+
+]
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context={
+		'posts':posts
+	}
+	return render(request, 'blog/home.html',context)
 
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html',{'title': 'about'})
+
+
+
+
